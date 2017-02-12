19e0e70e059fce91f113184749e9591313eb9c95
himanshi <himanshikhandelwal8@gmail.com>
2017-02-12 23:51:20 +0530

    comments added

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,18 @@
 from urllib import quote_plus
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render, get_object_or_404,redirect,render_to_response,redirect
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .forms import PostForm,UserLoginForm,UserRegisterForm
 from .models import Post
+from comments.forms import CommentForm
+from comments.models import Comment
 from django.contrib.auth import (authenticate,get_user_model,login,logout,)
 from django.contrib.auth.decorators import login_required
+from .utils import get_read_time
 
 def base(request):
     context = RequestContext(request)
@@ -50,7 +54,7 @@ def register_view(request):
     }
     return render(request,"login.html",context)
 
-
+@login_required(login_url='/login')
 def post_create(request):
     form = PostForm(request.POST or None,request.FILES or None)
     if form.is_valid():
@@ -68,12 +72,22 @@ def post_create(request):
 def post_detail(request,id): #retrieve
     instance = get_object_or_404(Post, id=id)
     share_string=quote_plus(instance.content)
+    comment_form=CommentForm(request.POST)
+    if comment_form.is_valid():
+        print(comment_form.cleaned_data)
+
     baseurl = request.build_absolute_uri()
+    content_type = ContentType.objects.get_for_model(Post)
+    obj_id = instance.id
+    comments = Comment.objects.filter(content_type=content_type, object_id=obj_id)
+
     context = {
         "title": instance.title,
         "instance": instance,
         "baseurl" :baseurl,
-        "share_string":share_string,
+        "share_string" : share_string,
+        "comments" : comments,
+        "comment_form" :comment_form,
     }
     return render(request, "post_detail.html", context)
 
